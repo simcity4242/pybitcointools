377c18362b67139c600920ce9f42f23cc419ea7c
wizard of ozzie <simcity4242@users.noreply.github.com>
2015-05-19 12:52:04 +1000

    Update __init__.py

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -1,5 +1,4 @@
-from bitcoin.py2specials import *
-from bitcoin.py3specials import *
+from bitcoin.pyspecials import *
 from bitcoin.main import *
 from bitcoin.transaction import *
 from bitcoin.deterministic import *
@@ -7,3 +6,4 @@ from bitcoin.bci import *
 from bitcoin.composite import *
 from bitcoin.stealth import *
 from bitcoin.blocks import *
+from bitcoin.mnemonic import *
