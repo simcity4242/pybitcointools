c83bc6192d09f510f2bb3f2acb5f7194495a27a5
wizard of ozzie <simcity4242@users.noreply.github.com>
2015-06-19 15:50:05 +1000

    Update __init__.py
    remove ios

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -8,7 +8,3 @@ from bitcoin.stealth import *
 from bitcoin.blocks import *
 from bitcoin.mnemonic import *
 from bitcoin.file_insert import *
-try:
-  from bitcoin.ios import *
-except ImportError:
-  pass
